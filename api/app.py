3f117539a22502995950122129a0d0bf47d1050c
Wladimir Lct <wlad.lucet@gmail.com>
2024-11-25 10:10:02 +0100

    feat(api): add initial API skeleton

--- api/app.py
+++ api/app.py
@@ -2,6 +2,18 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route("/baskets")
+def baskets():
+    return jsonify({"message": "Basket list"})
+
+@app.route("/items")
+def items():
+    return jsonify({"message": "Items list"})
+
+@app.route("/users")
+def users():
+    return jsonify({"message": "Users list"})
+
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to the Shop API!"})
