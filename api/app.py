d2fd9b6c04d7cada710d6526b041f73e9a11f80d
Wladimir Lct <wlad.lucet@gmail.com>
2024-11-26 17:35:11 +0100

    feat(api): Minimum API working

--- api/app.py
+++ api/app.py
@@ -1,18 +1,46 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+# Get the MongoDB URL from the environment variables
+mongo_url = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
+# Connect to mongodb 
+client = MongoClient(mongo_url)
+
 @app.route("/baskets")
 def baskets():
-    return jsonify({"message": "Basket list"})
+    # Return the list of baskets from the database
+    db = client["projet_cloud"]
+    baskets = db["baskets"]
+    result = list(baskets.find())
+    # Make ObjectID serializable
+    for basket in result:
+        basket["_id"] = str(basket["_id"])
+    return jsonify(result)
 
 @app.route("/items")
 def items():
-    return jsonify({"message": "Items list"})
+    # Return the list of items from the database
+    db = client["projet_cloud"]
+    items = db["items"]
+    result = list(items.find())
+    # Make ObjectID serializable
+    for basket in result:
+        basket["_id"] = str(basket["_id"])
+    return jsonify(result)
 
 @app.route("/users")
 def users():
-    return jsonify({"message": "Users list"})
+    # Return the list of users from the database
+    db = client["projet_cloud"]
+    users = db["users"]
+    result = list(users.find())
+    # Make ObjectID serializable
+    for basket in result:
+        basket["_id"] = str(basket["_id"])
+    return jsonify(result)
 
 @app.route("/")
 def home():
